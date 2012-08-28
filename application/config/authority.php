83d44cee5f08c53e321c20c2c99a7b38bfe9eea3
Charles Desneuf <c.desneuf@gmail.com>
2012-08-29 00:29:14 +0200

    Add rules for application(users and videos management)

--- application/config/authority.php
+++ application/config/authority.php
@@ -24,18 +24,16 @@ return array(
         // If a user doesn't have any roles, we don't have to give him permissions so we can stop right here.
         if(count($user->roles) === 0) return false;
 
+        if($user->has_role('video_moderater')){
+            /* User can update and delete video */
+            Authority::allow('moderate', 'video');
+        }
+
         if($user->has_role('admin'))
         {
             // The logged in user is an admin, we allow him to perform manage actions (create, read, update, delete) on "all" "Resources".
             Authority::allow('manage', 'all');
-
-            // Let's say we want to "Deny" the admin from adding accounts if his age is below 21 (i don't mean to discriminate ;) 
-            // Since we have the User object, and it has an "age" property, we can make a simple if statement.
-            if($user->age < 21)
-            {
-                // Too young! we "deny" the user to create users, i'm sorry...
-                Authority::deny('create', 'User');
-            }
+            Authority::allow('moderate', 'all');
 
             // Let's make it a little harder, we don't want the admin to be able to delete his own User account, but has to be allowed to delete other Users.
             // We only know that the "Resource" is a User, But we don't know the User id, we can send that information to the Rule Closure, in the Closure below, the argument is called $that_user.
@@ -47,27 +45,10 @@ return array(
             });
         }
 
-        if($user->has_role('store_owner'))
-        {
-            // What if the logged in User has the role "store_owner", let's allow the user to manage his own store
-            Authority::allow('manage', 'Store', function($store) use ($user)
-            {
-                return is_null(DB::table('stores')->where_id($store->id)->where_user_id($user->id)->first());
-            });
-
-            // We can also allow "Actions" on certain "Resources" by results we get from somewhere else, look closely at the next example
-            foreach(DB::table('permissions')->where_user_id($user->id)->get() as $permission)
-            {
-                if($permission->type === 'allow')
-                {
-                    Authority::allow($permission->action, $permission->resource);
-                }
-                else
-                {
-                    Authority::deny($permission->action, $permission->resource);    
-                }
-            }
-        }
+        /* An user can't unset his admin role */
+        Authority::deny('unset_admin_role', 'user', function($that_user) use ($user){
+            return (int)$that_user->id === (int)$user->id;
+        });
     }
 
 );
