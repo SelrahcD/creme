eff37f7b3bcb03af364e7e9ea92b64b1f24d34bf
Charles Desneuf <c.desneuf@gmail.com>
2012-08-29 19:02:43 +0200

    Message if no video

--- application/views/videos/watch.blade.php
+++ application/views/videos/watch.blade.php
@@ -1,3 +1,7 @@
+@if(isset($artist) && isset($title))
 <h1>{{ $artist }} - {{ $title }}</h1>
 
 <iframe src="{{ $url }}" frameborder="0" allowfullscreen></iframe>
+@else 
+<p>Pas de vidéo à afficher.</p>
+@endif
